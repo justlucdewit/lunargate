b9f42d6fe609d721be363b9d33c2f12880b1cd13
Luc de wit <luc.dewit@hotmail.nl>
2019-10-17 21:26:34 +0200

    initial commit

---
+++ interpreter.js
@@ -0,0 +1,7 @@
+function startUp() {
+    console.log("lunargate interpreter 0.1");
+    console.log("developed by luc de wit");
+}
+function interpreter() {
+    //yugios stuff goes here
+}
---
+++ interpreter.ts
@@ -0,0 +1,8 @@
+function startUp(){
+    console.log("lunargate interpreter 0.1")
+    console.log("developed by luc de wit")
+}
+
+function interpreter(){
+    //yugios stuff goes here
+}
